c21dc373a9d2984e2e784b42c5144402d00817f3
unknown <https://github.com/omaesayed21>
2025-06-27 12:26:25 +0300

    Erorr Handling in Todolist

--- src/Componets/ui/TodoList.tsx
+++ src/Componets/ui/TodoList.tsx
@@ -1,58 +1,58 @@
 import { useEffect, useState } from "react"
 import Button from "./Button"
 import axiosInstance from "../../config/axios.config"
+import { useQuery } from "@tanstack/react-query";
 
 const TodoList = () =>{
-    const [todos , setTodos] = useState([]);
 
         const userDataString = localStorage.getItem("loggedInUser");
         const userData = userDataString ? JSON.parse(userDataString) : null;
       
-    useEffect(() => {
-        try {
-         axiosInstance.get('/users/me?populate=todos', {
-            headers:{
-                Authorization: `Bearer ${userData.jwt}`
-            }
-         }
-         
-         ).then((res) => 
-            setTodos(res.data.todos))
-            .catch((error) => {
-            console.log( "The Error" , error);
-         })
-        }
-        catch (error) {
-            console.log(error);
-            
-        }
+   const {isLoading , data} =  useQuery({
 
-    }, [
-        userData.jwt
-    ])
+    queryKey: ["todos"],
+    queryFn :async () =>{
 
+   const {data} =  await axiosInstance.get("/users/me?populate=todos" , {
+        headers : {
+            Authorization : `Bearer ${userData.jwt}`
+        }
+    })
+    return data
+     
+   } 
+   })
+console.log(data);
+
+
+
+if(isLoading){
+    return <div>Loading...</div>
+}
 
 
     return <>
     <div className=" space-y-1">
-
-        {
-            todos.map(todos =>(
-                <div key={todos.id} className=" flex justify-between items-center  hover:bg-slate-200  duration-300 rounded-md p-2">
-                <p  className=" w-full font-semibold">{todos.title}</p>
-                <div className=" flex gap-2 items-center justify-end  space-x-3">
-                    <Button  size={"sm"}>Edit</Button>
-                    <Button variant={"danger"} size={"sm"}>Remove</Button>
-                </div>
-    
+    { data.todos.legnth  ?
+        data.todos.map(todo => (
+            <div key={todo.id} className=" flex justify-between items-center  hover:bg-slate-200  duration-300 rounded-md p-2">
+            <p  className=" w-full font-semibold">{todo.title}</p>
+            <div className=" flex gap-2 items-center justify-end  space-x-3">
+                <Button  size={"sm"}>Edit</Button>
+                <Button variant={"danger"} size={"sm"}>Remove</Button>
             </div>
-            ))
-        }
 
-      
+        </div>
+        ))
+    :
+        <p className=" text-center text-gray-400">No todos found</p>
+        }   
+        
+            
+            
 
     </div>
     
     </>
 }
-export default TodoList
+export default TodoList 
