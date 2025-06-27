713eb7ac00d78b686f364346d954168d6c8998f8
unknown <https://github.com/omaesayed21>
2025-06-27 10:22:27 +0300

    fetch Todo List

--- src/Componets/ui/TodoList.tsx
+++ src/Componets/ui/TodoList.tsx
@@ -1,16 +1,55 @@
+import { useEffect, useState } from "react"
 import Button from "./Button"
+import axiosInstance from "../../config/axios.config"
 
 const TodoList = () =>{
-    return <>
-    <div className=" space-y-4">
-        <div className=" flex justify-between items-center  hover:bg-slate-200  duration-300 rounded-md p-2">
-            <p  className=" w-full font-semibold"> First Todo</p>
-            <div className=" flex gap-2 items-center justify-end  space-x-3">
-                <Button variant={"cancel"} size={"sm"}>Edit</Button>
-                <Button variant={"danger"} size={"sm"}>Delete</Button>
-            </div>
+    const [todos , setTodos] = useState([]);
 
-        </div>
+        const userDataString = localStorage.getItem("loggedInUser");
+        const userData = userDataString ? JSON.parse(userDataString) : null;
+      
+    useEffect(() => {
+        try {
+         axiosInstance.get('/users/me?populate=todos', {
+            headers:{
+                Authorization: `Bearer ${userData.jwt}`
+            }
+         }
+         
+         ).then((res) => 
+            setTodos(res.data.todos))
+            .catch((error) => {
+            console.log( "The Error" , error);
+         })
+        }
+        catch (error) {
+            console.log(error);
+            
+        }
+
+    }, [
+        userData.jwt
+    ])
+
+
+
+    return <>
+    <div className=" space-y-1">
+
+        {
+            todos.map(todos =>(
+                <div key={todos.id} className=" flex justify-between items-center  hover:bg-slate-200  duration-300 rounded-md p-2">
+                <p  className=" w-full font-semibold">{todos.title}</p>
+                <div className=" flex gap-2 items-center justify-end  space-x-3">
+                    <Button  size={"sm"}>Edit</Button>
+                    <Button variant={"danger"} size={"sm"}>Remove</Button>
+                </div>
+    
+            </div>
+            ))
+        }
+
+      
 
     </div>
     
